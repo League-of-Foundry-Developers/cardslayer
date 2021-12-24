f2eadb54d1b5b66e4bc2e2d777907d59c53cce81
Anathema <shane.martland@gmail.com>
2021-12-23 17:59:47 -0800

    Add commenting, Switch from using array to store data to object

--- scripts/card-slayer.js
+++ scripts/card-slayer.js
@@ -1,4 +1,13 @@
+/**
+ * ES6 module imports
+ */
+
 import CardSlayer from "./CardSlayer.js";
+
+/**
+ * Hook on initialization to establish the cardslayer as a
+ * viable canvas layer to store the display data for placed cards
+ */
 
 Hooks.on("init", () => {
 	CONFIG.Canvas.layers["cardslayer"] = {
@@ -7,10 +16,18 @@ Hooks.on("init", () => {
 	};
 });
 
+/**
+ * Hook on ready to check if the scene has an established
+ * set of cardslayer flags, if it doesn't, create an empty
+ * object for one.
+ */
+
+
 Hooks.on("ready", () => {
 
 	if (!(canvas.scene.getFlag("card-slayer", "cards"))) {
-		canvas.scene.setFlag("card-slayer", "cards", [])
+		canvas.scene.setFlag("card-slayer", "cards", {})
 	}
+
 })
 
